d334e1a23561dc77b9cbe00d91231cb3594eb4bc
Mastrojanni <agrassi2135n@gmail.com>
2021-12-05 18:07:40 +0100

    Add node-sass(scss)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// style
+import './App.scss';
+
+
+const App = () => {
+
+  return (
+    <div className="app__container">
+      <header></header>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
